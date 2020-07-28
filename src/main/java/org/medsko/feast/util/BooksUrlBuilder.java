bd0fdbcbf895c5fa2bb521a28b5da8b68559ee62
Melle de Vries <melle@openweb.nl>
2020-07-28 22:29:37 +0200

    Add javadoc for UrlBuilder for books API

--- src/main/java/org/medsko/feast/util/BooksUrlBuilder.java
+++ src/main/java/org/medsko/feast/util/BooksUrlBuilder.java
@@ -19,11 +19,24 @@ public class BooksUrlBuilder extends AbstractUrlBuilder<BooksUrlBuilder> {
         return this;
     }
 
+    /**
+     * By default, Open Library returns JSON specifying a couple of URLs: a field info_url (to book page in Open Library)
+     * and a preview_url. This might be handy if redirecting the user to Open Library is the goal - so far my intentions
+     * are more scrapey than that.
+     * <p>
+     * Since the format of the response is tightly coupled to the presence of this parameter, perhaps some construction
+     * that mirrors this dependency is in order.
+     * @return {@code this}
+     */
     public BooksUrlBuilder asData() {
         appendParameter(DATA_FORMAT_PARAMETER);
         return this;
     }
 
+    /**
+     * By default, Open Library will return javascript. You probably want to call this method.
+     * @return {@code this}
+     */
     public BooksUrlBuilder asJson() {
         appendParameter(JSON_FORMAT_PARAMETER);
         return this;
